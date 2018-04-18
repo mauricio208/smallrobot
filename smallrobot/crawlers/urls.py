ce0af1bff2bc783c0619649b882aeb86274412cf
Mauricio <mauricio208@gmail.com>
2018-04-17 21:41:54 -0400

    security update

--- smallrobot/crawlers/urls.py
+++ smallrobot/crawlers/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
+from django.views.generic import RedirectView
 
 from . import views
 
 app_name = 'crawlers'
 urlpatterns = [
-    path(r'list', views.CrawlersList.as_view(), name='crawlers_list'),
+    path('',RedirectView.as_view(pattern_name='crawlers:crawlers_list')),
+    path('list', views.CrawlersList.as_view(), name='crawlers_list'),
     path('run', views.crawler_runner, name='crawler_run'),
     path('lastresult', views.crawler_last_result, name='crawler_last_result'),
 ]
